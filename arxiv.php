1b7275864f005e55ebd5de600205f6df09240251
Nguyễn Hồ Duy Tri <duytri1009@gmail.com>
2016-10-23 15:42:47 +0700

    Del category var, fix output path and change request url

--- arxiv.php
+++ arxiv.php
@@ -3,27 +3,30 @@
 //File input các chủ đề cần search và crawl về trên website arXiv.org
 $input_file = fopen("input_subject.txt", "r") or die("Unable to open input file!");
 //Chủ đề cần tìm
-$subject_category = "Computer Science";
+//$subject_category = "Computer Science";
 //Đường dẫn thư mục lưu trữ dữ liệu, nằm trong thư mục gốc
 $output_dir = "output";
+//Dấu phân cách thư mục
+$slash = "\\";
 //Thời gian giãn cách giữa các request (giây)
-$delay_time = 3;
+$delay_time = 5;
 ////////////////////////////////////////////////////
 
 
 if (!file_exists($output_dir)) {
-    mkdir($output_dir, 0777, true);
+    mkdir($output_dir, 777, true);
 }
-chdir($output_dir);
+
 while (!feof($input_file)) {
-	$subject = fgets($input_file);
+	$subject = trim(fgets($input_file));
 	print_r($subject);
-	$url = 'http://export.arxiv.org/api/query?search_query=all:%22'.$subject.'%22+AND+cat:%22'.$subject_category.'%22&start=0&max_results=100&sortBy=lastUpdatedDate&sortOrder=descending';
+	//$url = 'http://export.arxiv.org/api/query?search_query=all:%22'.$subject.'%22+AND+cat:%22'.$subject_category.'%22&start=0&max_results=100&sortBy=lastUpdatedDate&sortOrder=descending';
+	$url = 'http://export.arxiv.org/api/query?search_query=all:%22'.str_replace(' ', '+', $subject).'%22&start=0&max_results=100&sortBy=lastUpdatedDate&sortOrder=descending';
 	$response = file_get_contents($url);
-	$output_file = fopen($subject, "w") or die("Unable to create file \'".$subject."\' !");
+	print_r($response);
+	$output_file = fopen(getcwd().$slash.$output_dir.$slash.str_replace(' ', '', ucwords($subject)).'.txt', "w") or die("Unable to create file for subject '".$subject."' !");
 	fwrite($output_file, $response);
 	fclose($output_file);
-	print_r($response);
 	sleep($delay_time);
 }
 fclose($input_file);
