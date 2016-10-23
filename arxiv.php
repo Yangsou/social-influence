1b4f204ada9ee84e73669309cecd43577fabafa3
Nguyễn Hồ Duy Tri <duytri1009@gmail.com>
2016-10-23 14:18:36 +0700

    Add category arg and sort to query

--- arxiv.php
+++ arxiv.php
@@ -2,10 +2,12 @@
 //Các thông số đầu vào:
 //File input các chủ đề cần search và crawl về trên website arXiv.org
 $input_file = fopen("input_subject.txt", "r") or die("Unable to open input file!");
+//Chủ đề cần tìm
+$subject_category = "Computer Science";
 //Đường dẫn thư mục lưu trữ dữ liệu, nằm trong thư mục gốc
 $output_dir = "output";
 //Thời gian giãn cách giữa các request (giây)
-$delay_time = 15;
+$delay_time = 3;
 ////////////////////////////////////////////////////
 
 
@@ -15,7 +17,8 @@ if (!file_exists($output_dir)) {
 chdir($output_dir);
 while (!feof($input_file)) {
 	$subject = fgets($input_file);
-	$url = 'http://export.arxiv.org/api/query?search_query=all:'.$subject.'&start=0&max_results=100';
+	print_r($subject);
+	$url = 'http://export.arxiv.org/api/query?search_query=all:%22'.$subject.'%22+AND+cat:%22'.$subject_category.'%22&start=0&max_results=100&sortBy=lastUpdatedDate&sortOrder=descending';
 	$response = file_get_contents($url);
 	$output_file = fopen($subject, "w") or die("Unable to create file \'".$subject."\' !");
 	fwrite($output_file, $response);
